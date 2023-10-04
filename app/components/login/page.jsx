390a19e9015ec9761a531199fda6cd3ddc359b4f
buitannguyen2612 <buitannguyen26122001@gmail.com>
2023-10-04 21:59:19 +0700

    fix login

--- app/components/login/page.jsx
+++ app/components/login/page.jsx
@@ -1,12 +1,13 @@
-"use client"; 
+"use client";
 import React, { useState } from "react";
-import { Button, TextField } from "@mui/material";
-import "./page.css";
+import { Box, Button, TextField } from "@mui/material";
+import styles from './page.module.css'
 
 export default function Login(props) {
 
   const [userName, setUserName] = useState('')
   const [password, setPassword] = useState('')
+
 
 
   // Ham check cac chu viet hoa tra ve true
@@ -54,40 +55,60 @@ export default function Login(props) {
     e.preventDefault()
   }
 
+
   return (
-    <div className="container_login">
-      <div className="login_box">
-        <span className="login__span username">
-          <TextField
-            error={userValidation() == true}
-            className="input"
-            id="filled-username"
-            label="UserName"
-            type="search"
-            variant="filled"
-            onChange={e => setUserName(e.target.value)}
-            helperText={userValidation() ? 'only contain normal text' : ''}
-          />
-        </span>
-        <span className="login__span psw">
-          <TextField
-            error={passValida() == true}
-            className="input"
-            id="filled-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            variant="filled"
-            onChange={e => setPassword(e.target.value)}
-            helperText={passValida() ? 'psw cant be empty or have space' : ''}
-          />
-        </span>
-        <div className="login_btn">
+    <Box sx={{
+      height: '100vh',
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      marginTop: '30px',
+    }}
+    >
+      <Box sx={{
+        height: '50%',
+        width: '25%',
+        display: 'flex',
+        flexDirection: 'column',
+        boxShadow: '#c5c5c5 0px 2px 8px 3px',
+        borderRadius: '10px',
+        gap: '10px',
+        padding: '18px 18px',
+        backgroundColor: 'white',
+      }}>
+        <TextField
+          sx={{
+            width: '100%',
+            userSelect: 'none',
+          }}
+          error={userValidation() == true}
+          id="filled-username"
+          label="UserName"
+          type="search"
+          variant="filled"
+          onChange={e => setUserName(e.target.value)}
+          helperText={userValidation() ? 'only contain normal text' : ''}
+        />
+        <TextField
+          error={passValida() == true}
+          sx={{
+            width: '100%',
+            userSelect: 'none',
+          }}
+          id="filled-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="filled"
+          onChange={e => setPassword(e.target.value)}
+          helperText={passValida() ? 'psw cant be empty or have space' : ''}
+        />
+        <Box className={styles['login-btn']}>
           <Button variant="contained" onClick={e => submitLogin(e)}>Login</Button>
           <Button variant="contained" onClick={e => submitLogin(e)}>forgot password</Button>
-        </div>
-          <a href="#">register</a>
-      </div>
-    </div>
+        </Box>
+        <a href="#">register</a>
+      </Box>
+    </Box>
   );
 }
