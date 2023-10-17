4b7f981c39635d5ae02bb401eb06eabcbb8525b4
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-10-17 12:08:12 +0700

    fix: removed course object & import mock data file

--- app/user/invoice/page.js
+++ app/user/invoice/page.js
@@ -3,63 +3,7 @@ import * as React from 'react'
 import CourseCard from '@/app/components/UserPage/CourseCard'
 import CoursePagination from '@/app/components/UserPage/CoursePagination'
 import { Box } from '@mui/material'
-
-const courses = [
-  {
-    id: 1,
-    image: 'https://images.unsplash.com/photo-1696958363946-b58f40cda426',
-    title: 'Next 13 Crash Course',
-    categories: ['Frontend', 'Nextjs'],
-    description:
-      'Grabbed lying yourself stream boy possible fairly worth oldest track baseball smallest guess forth kill column. Manufacturing plates pan son straw talk had bent orange thank buried round adult push birth doll.',
-    price: 120,
-  },
-  {
-    id: 2,
-    image: 'https://images.unsplash.com/photo-1696958363946-b58f40cda426',
-    title: 'Next 13 Crash Course',
-    categories: ['Frontend', 'Nextjs'],
-    description:
-      'Grabbed lying yourself stream boy possible fairly worth oldest track baseball smallest guess forth kill column. Manufacturing plates pan son straw talk had bent orange thank buried round adult push birth doll.',
-    price: 200,
-  },
-  {
-    id: 3,
-    image: 'https://images.unsplash.com/photo-1696958363946-b58f40cda426',
-    title: 'Next 13 Crash Course',
-    categories: ['Frontend', 'Nextjs'],
-    description:
-      'Grabbed lying yourself stream boy possible fairly worth oldest track baseball smallest guess forth kill column. Manufacturing plates pan son straw talk had bent orange thank buried round adult push birth doll.',
-    price: 340,
-  },
-  {
-    id: 4,
-    image: 'https://images.unsplash.com/photo-1696958363946-b58f40cda426',
-    title: 'Next 13 Crash Course',
-    categories: ['Frontend', 'Nextjs'],
-    description:
-      'Grabbed lying yourself stream boy possible fairly worth oldest track baseball smallest guess forth kill column. Manufacturing plates pan son straw talk had bent orange thank buried round adult push birth doll.',
-    price: 340,
-  },
-  {
-    id: 5,
-    image: 'https://images.unsplash.com/photo-1696958363946-b58f40cda426',
-    title: 'Next 13 Crash Course',
-    categories: ['Frontend', 'Nextjs'],
-    description:
-      'Grabbed lying yourself stream boy possible fairly worth oldest track baseball smallest guess forth kill column. Manufacturing plates pan son straw talk had bent orange thank buried round adult push birth doll.',
-    price: 340,
-  },
-  {
-    id: 6,
-    image: 'https://images.unsplash.com/photo-1696958363946-b58f40cda426',
-    title: 'Next 13 Crash Course',
-    categories: ['Frontend', 'Nextjs'],
-    description:
-      'Grabbed lying yourself stream boy possible fairly worth oldest track baseball smallest guess forth kill column. Manufacturing plates pan son straw talk had bent orange thank buried round adult push birth doll.',
-    price: 340,
-  },
-]
+import { courses } from '@/app/utils/user.data'
 
 const InvoicePage = () => {
   const itemsPerPage = 3
