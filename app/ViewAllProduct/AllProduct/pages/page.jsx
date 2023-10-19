9e5f2d82958e400ff88ea414f7664e4de001b91c
buitannguyen2612 <buitannguyen26122001@gmail.com>
2023-10-19 17:51:22 +0700

    all done viewall

--- app/ViewAllProduct/AllProduct/pages/page.jsx
+++ app/ViewAllProduct/AllProduct/pages/page.jsx
@@ -1,21 +1,22 @@
 "use client"
-import { Box, Pagination, Typography } from '@mui/material'
+import { Box, FormControl, InputLabel, MenuItem, Pagination, Select, Stack, Typography } from '@mui/material'
 import React, { useState } from 'react'
 import ListAllProduct from '../components/listAllProduct/page'
-import { GetAllData } from './callAPI'
+import { GetAllData, GetData, GetDataByCategory } from './callAPI'
 
 export default function AllCourseUser() {
     const [currentPage, setCurrentPage] = useState(1);
+    const [category, setCategory] = useState(0)
 
     const itemsPerPage = 8
 
-    const data = GetAllData().data
+    let getAllDaTa = GetData(category)
 
     // xac dinh so trang
-    const pageCount = Math.ceil(data.length / itemsPerPage);
+    const pageCount = Math.ceil(getAllDaTa.length / itemsPerPage);
 
     // xac dinh san pham nao se duoc render
-    const displayedData = data.slice(
+    const displayedData = getAllDaTa.slice(
         (currentPage - 1) * itemsPerPage,
         ((currentPage - 1) + 1) * itemsPerPage
     );
@@ -25,9 +26,38 @@ export default function AllCourseUser() {
     const handlePagnating = (e, vl) => {
         setCurrentPage(vl)
     }
+
+    // set category my choose
+    const handleCategory = (e) =>{
+        setCategory(e.target.value)
+    }
+    
     return (
         <Box sx={{ height: 'auto', width: "100%", marginTop: "30px", display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
-            <Typography variant='h1' sx={{ fontSize: '30px', color: '#252424', fontWeight: '800' }}>All course contain</Typography>
+            <Stack
+                direction="column"
+                spacing={2}
+            >
+                <Typography variant='h1' sx={{ fontSize: '30px', color: '#252424', fontWeight: '800' }}>All course contain</Typography>
+                <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                    <InputLabel id="demo-simple-select-standard-label">Search By Category</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-standard-label"
+                        id="demo-simple-select-standard"
+                        value={category}
+                        onChange={handleCategory}
+                        label="Category"
+                        defaultValue={0}
+                    >
+                        <MenuItem value={0}>All Course</MenuItem>
+                        <MenuItem value={1}>Case 1</MenuItem>
+                        <MenuItem value={2}>Case 2</MenuItem>
+                        <MenuItem value={3}>Case 3</MenuItem>
+                        <MenuItem value={4}>Case 4</MenuItem>
+                        <MenuItem value={5}>Case 5</MenuItem>
+                    </Select>
+                </FormControl>
+            </Stack>
             <ListAllProduct displayedData={displayedData} />
             <Pagination count={pageCount} color="secondary" onChange={handlePagnating} sx={{ marginTop: "30px", width: "100%", display: "flex", justifyContent: "center" }} />
         </Box>
--- app/ViewAllProduct/page.jsx
+++ app/ViewAllProduct/page.jsx
@@ -4,6 +4,7 @@ import OwnProduct from './OwnProduct/pages/page'
 import SliderHero from './SliderHero/page'
 import AllCourseUser from './AllProduct/pages/page'
 import OurMember from './OurMember/page'
+import FootView from './footViewAll/page'
 
 export default function page() {
   return (
@@ -12,6 +13,7 @@ export default function page() {
       <OwnProduct/>
       <OurMember/>
       <AllCourseUser/>
+      <FootView/>
     </Box>
   )
 }
