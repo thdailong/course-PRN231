7f73989b9545c2178e53f42a6b8fc9e8d14c8efd
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-10-31 23:50:24 +0700

    feat: handle auth redux

--- app/redux/reducers/user.js
+++ app/redux/reducers/user.js
@@ -1,27 +1,38 @@
-import { ACCESS_TOKEN } from '@/app/constant/constant'
-import { eraseCookie, readCookie } from '@/app/utils/cookies'
+import { ACCESS_TOKEN, ROLE } from '@/app/constant/constant'
+import { createCookie, eraseCookie, readCookie } from '@/app/utils/cookies'
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = () => {
-  let access_token = readCookie('access_token')
+  let access_token = readCookie(ACCESS_TOKEN)
   return {
     isLogin: access_token ? true : false,
+    error: null,
   }
 }
 
-const userReducers = createSlice({
+const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
     login: (state, action) => {
+      const days = new Date(action.payload.expiredIn).getDay()
+      createCookie(ACCESS_TOKEN, action.payload.accessToken, days)
+      createCookie(ROLE, action.payload.role)
       state.isLogin = true
+      state.error = null
     },
-    logout: (state, action) => {
-      eraseCookie(ACCESS_TOKEN)
+    loginFailure: (state, action) => {
+      state.error = action.payload
       state.isLogin = false
+    },
+    logout: (state) => {
+      eraseCookie(ACCESS_TOKEN)
+      eraseCookie(ROLE)
+      state.isLogin = false
+      state.error = null
     },
   },
 })
 
-export const { login, logout } = userReducers.actions
-export default userReducers.reducer
+export const userActions = userSlice.actions
+export default userSlice.reducer
