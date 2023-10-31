e4c26b0271dd6c6d2f94151e03065624e4e28537
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-10-31 23:45:22 +0700

    feat: handle login

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -1,77 +1,53 @@
-"use client";
-import React, { useState } from "react";
-import { Box, Button, TextField } from "@mui/material";
-import styles from './page.module.css'
-import Layout from "../layout";
-import Link from "next/link";
+'use client'
+import styles from '@/app/auth/login/page.module.css'
+import { isPasswordValid, isUserValid } from '@/app/auth/utils/utils'
+import useActions from '@/app/hooks/useActions'
+import useShallowEqualSelector from '@/app/hooks/useShallowEqualSelector'
+import { userActions } from '@/app/redux/reducers/user'
+import * as auth from '@/app/rest_client/authClient'
+import { Box, Button, TextField } from '@mui/material'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
-export default function Login(props) {
-
+export default function Login() {
   const [userName, setUserName] = useState('')
   const [password, setPassword] = useState('')
 
-
+  const { login, loginFailure } = useActions(userActions)
   const router = useRouter()
+  const { isLogin } = useShallowEqualSelector((state) => state.user)
 
-
-  // Ham check cac chu viet hoa tra ve true
-  const containsUppercase = (str) => {
-    return /[A-Z]/.test(str);
-  }
-
-
-  // Ham check ky tu dac biet
-  const checkSpecialSymbol = (str) => {
-    return /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(str)
-  }
-
-
-  // ham check space
-  const checkSpace = (str) => {
-    return /[ ]/.test(str);
-  }
-
-
-  // ham check validation cua username
-  const userValidation = () => {
-    if (containsUppercase(userName) || checkSpecialSymbol(userName) || userName.length === 0) {
-      return true
+  useEffect(() => {
+    if (isLogin) {
+      router.push('/home')
     }
-    if (!containsUppercase(userName) && !checkSpecialSymbol(userName) || !userName.length === 0) {
-      return false
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLogin])
+
+  const submitLogin = async () => {
+    if (!isLogin) {
+      try {
+        const res = await auth.login({ userName, password })
+        login(res.data)
+      } catch (error) {
+        loginFailure(error.message)
+      }
     }
   }
-
-
-  //ham check validation cua password
-  function passValida() {
-    if (password.length === 0 || checkSpace(password)) {
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
-
-
-  //ham handle click login button
-  const submitLogin = (e) => {
-    e.preventDefault()
-    router.push('/auth/FogotPassword')
-  }
-
 
   return (
-      <Box sx={{
+    <Box
+      sx={{
         height: '100vh',
         width: '100%',
         display: 'flex',
         justifyContent: 'center',
         marginTop: '30px',
       }}
-      >
-        <Box sx={{
+    >
+      <Box
+        sx={{
           height: '50%',
           width: '25%',
           display: 'flex',
@@ -81,41 +57,50 @@ export default function Login(props) {
           gap: '10px',
           padding: '18px 18px',
           backgroundColor: 'white',
-        }}>
-          <TextField
-            sx={{
-              width: '100%',
-              userSelect: 'none',
-            }}
-            error={userValidation() == true}
-            id="filled-username"
-            label="UserName"
-            type="search"
-            variant="filled"
-            onChange={e => setUserName(e.target.value)}
-            helperText={userValidation() ? 'only contain normal text' : ''}
-          />
-          <TextField
-            error={passValida() == true}
-            sx={{
-              width: '100%',
-              userSelect: 'none',
-            }}
-            id="filled-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            variant="filled"
-            onChange={e => setPassword(e.target.value)}
-            helperText={passValida() ? 'psw cant be empty or have space' : ''}
-          />
-          <Box className={styles['login-btn']}>
-            <Button variant="contained">Login</Button>
-            <Button variant="contained" onClick={e => submitLogin(e)}>forgot password</Button>
-          </Box>
-          <Link href="/auth/Register">register</Link>
+        }}
+      >
+        <TextField
+          sx={{
+            width: '100%',
+            userSelect: 'none',
+          }}
+          error={isUserValid(userName) == true}
+          id="filled-username"
+          label="UserName"
+          type="search"
+          variant="filled"
+          onChange={(e) => setUserName(e.target.value)}
+          helperText={isUserValid(userName) ? 'only contain normal text' : ''}
+        />
+        <TextField
+          error={isPasswordValid(password) == true}
+          sx={{
+            width: '100%',
+            userSelect: 'none',
+          }}
+          id="filled-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="filled"
+          onChange={(e) => setPassword(e.target.value)}
+          helperText={
+            isPasswordValid(password) ? 'passsword cant be empty or have space' : ''
+          }
+        />
+        <Box className={styles['login-btn']}>
+          <Button variant="contained" onClick={submitLogin}>
+            Login
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => router.push('/auth/FogotPassword')}
+          >
+            Forgot password
+          </Button>
         </Box>
+        <Link href="/auth/register">Register</Link>
       </Box>
-
-  );
+    </Box>
+  )
 }
