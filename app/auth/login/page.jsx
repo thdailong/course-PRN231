9c061b7c2b1f9e4cc3e71bf7073f60b42fb69bff
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-11-02 09:47:32 +0700

    fix: height to minHeight

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -39,7 +39,7 @@ export default function Login() {
   return (
     <Box
       sx={{
-        height: '100vh',
+        minHeight: '100vh',
         width: '100%',
         display: 'flex',
         justifyContent: 'center',
